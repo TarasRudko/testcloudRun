83e924daf1c237eba9791c8462c3cb5f253ce0a9
Taras Rudko <taras.rudko@dyson.com>
2023-11-06 13:44:13 +0200

    Update requirements and db

--- Matema/settings.py
+++ Matema/settings.py
@@ -64,7 +64,7 @@ WSGI_APPLICATION = 'Matema.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('SQL_NAME'),
         'USER': config('SQL_USER'),
         'PASSWORD': config('SQL_PASSWORD'),
